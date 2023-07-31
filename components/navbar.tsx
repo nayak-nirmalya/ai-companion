ece1793a891ead9ac98ea9e88cd9ec0c01d82211
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-31 13:19:36 +0530

    added styling and userbtn to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,37 @@
 "use client";
 
+import { Menu } from "lucide-react";
 import React from "react";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { UserButton } from "@clerk/nextjs";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"]
+});
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            companion.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <UserButton />
+      </div>
+    </div>
+  );
 }
