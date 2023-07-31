484c28a311ebedcd61fc279ffc52a28cf09e03ce
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-31 12:44:40 +0530

    added UserButton to logout/manage account

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 export default function RootPage() {
-  return <div>RootPage (Protected!)</div>;
+  return (
+    <div>
+      RootPage (Protected!)
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
