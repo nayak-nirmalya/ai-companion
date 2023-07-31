bda5b36eccb6b5f7a00f67509a9f82b8b14f1644
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-31 12:43:10 +0530

    centered clerk auth modal

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AuthLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex items-center justify-center h-full">{children}</div>
+  );
+}
