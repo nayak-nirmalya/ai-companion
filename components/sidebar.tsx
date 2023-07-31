4b9996506ac27c75501dcd5a5bc9d61e2b91f422
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-31 14:24:33 +0530

    show routes on sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,55 @@
 "use client";
 
 import React from "react";
+import { Home, Plus, Settings } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const pathname = usePathname();
+
+  const routes = [
+    {
+      icon: Home,
+      href: "/",
+      label: "Home",
+      pro: false
+    },
+    {
+      icon: Plus,
+      href: "/companion/new",
+      label: "Create",
+      pro: true
+    },
+    {
+      icon: Settings,
+      href: "/settings",
+      label: "Settings",
+      pro: false
+    }
+  ];
+
+  return (
+    <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
+      <div className="p-3 flex-1 flex justify-center">
+        <div className="space-y-2">
+          {routes.map((route) => (
+            <div
+              key={route.href}
+              className={cn(
+                "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                pathname === route.href && "bg-primary/10 text-primary"
+              )}
+            >
+              <div className="flex flex-col gap-y-2 items-center flex-1">
+                <route.icon className="h-5 w-5" />
+                {route.label}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
