e797c673cda4c93e424fb0b04a49d9daa8aea085
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-31 19:45:04 +0530

    new component: Categories

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 import SearchInput from "@/components/search-input";
+import Categories from "@/components/categories";
 
-export default function RootPage() {
+import prismadb from "@/lib/prismadb";
+
+export default async function RootPage() {
+  const categories = await prismadb.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 }
---
+++ components/categories.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import { Category } from "@prisma/client";
+
+export default function Categories({ data }: { data: Category[] }) {
+  return <div>Categories</div>;
+}
