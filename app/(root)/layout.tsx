87c10700aec3d447f1f1d01cfcefdfaead5a5c50
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-31 14:17:02 +0530

    new component: Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
-import Navbar from "@/components/navbar";
 import React from "react";
+
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 
 export default function RootLayout({
   children
@@ -9,6 +11,9 @@ export default function RootLayout({
   return (
     <div className="h-full">
       <Navbar />
+      <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+        <Sidebar />
+      </div>
       <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
   );
