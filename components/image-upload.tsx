fb39fa7a04d8c997a6a72ff2a3fdc7ca0704acee
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-01 13:58:00 +0530

    added assets to public dir

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { CldUploadButton } from "next-cloudinary";
+import Image from "next/image";
 
 interface ImageUploadProps {
   value: string;
@@ -21,5 +23,20 @@ export const ImageUpload = ({
 
   if (!isMounted) return null;
 
-  return <div>ImageUpload</div>;
+  return (
+    <div className="space-y-4 w-full flex flex-col justify-center items-center">
+      <CldUploadButton uploadPreset="erdhba9i" options={{ maxFiles: 1 }}>
+        <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
+          <div className="relative h-40 w-40">
+            <Image
+              fill
+              alt="Upload"
+              src="/placeholder.svg"
+              className="rounded-lg object-cover"
+            />
+          </div>
+        </div>
+      </CldUploadButton>
+    </div>
+  );
 };
