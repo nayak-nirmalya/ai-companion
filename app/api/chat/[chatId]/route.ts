1d906ef9b1c70debcb68ff04ff150bba4dfd066d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-02 20:02:41 +0530

    installed dotenv pkg

---
+++ app/api/chat/[chatId]/route.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import { StreamingTextResponse, LangChainStream } from "ai";
+import { auth, currentUser } from "@clerk/nextjs";
+import { Replicate } from "langchain/llms/replicate";
+import { CallbackManager } from "langchain/callbacks";
+import { NextResponse } from "next/server";
+
+import { MemoryManager } from "@/lib/memory";
+import { ratelimit } from "@/lib/rate-limit";
+import prismadb from "@/lib/prismadb";
