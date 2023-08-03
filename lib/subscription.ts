dc6288272eb399b710a1715fb7643679370e29d9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-03 13:33:03 +0530

    new lib func to checkSubscription

---
+++ lib/subscription.ts
@@ -0,0 +1,32 @@
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
+const DAY_IN_MS = 86_400_000;
+
+export const checkSubscription = async () => {
+  const { userId } = auth();
+
+  if (!userId) return false;
+
+  const userSubscription = await prismadb.userSubscription.findUnique({
+    where: {
+      userId
+    },
+    select: {
+      stripeCurrentPeriodEnd: true,
+      stripeCustomerId: true,
+      stripePriceId: true,
+      stripeSubscriptionId: true
+    }
+  });
+
+  if (!userSubscription) return false;
+
+  const isValid =
+    userSubscription.stripePriceId &&
+    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+      Date.now();
+
+  return !!isValid;
+};
