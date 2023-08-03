9196ed07144ba4cc09f5d70fe229d884018e9835
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-03 13:38:21 +0530

    new utils function absoluteUrl

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,10 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/lib/utils";
+
+const settingsUrl = absoluteUrl("/settings");
+
+export async function GET() {}
