6c77ef9540292e95a5954093dda8289d0718f18a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-03 14:11:18 +0530

    new component: ProModal

---
+++ components/pro-modal.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { useToast } from "@/components/ui/use-toast";
+
+export default function ProModal() {
+  return <div>ProModal</div>;
+}
