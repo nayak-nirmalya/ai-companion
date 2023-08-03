b7c9373b32893426b0f6acb11799cc1a9ed2b9f0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-03 14:03:46 +0530

    added api/webhook to publicRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({ publicRoutes: ["/api/webhook"] });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
