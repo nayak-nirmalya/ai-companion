1dd65c1838b08390154927122ec1613926c078db
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-03 14:23:26 +0530

    bug fix: removed paypal stripe payment method

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -34,7 +34,7 @@ export async function GET() {
     const stripeSession = await stripe.checkout.sessions.create({
       success_url: settingsUrl,
       cancel_url: settingsUrl,
-      payment_method_types: ["card", "paypal"],
+      payment_method_types: ["card"],
       mode: "subscription",
       billing_address_collection: "auto",
       customer_email: user.emailAddresses[0].emailAddress,
